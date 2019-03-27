31b9910b20054861dde8b37991aadf8ad565460b
zzxaifj <1179278531@qq.com>
2019-03-27 11:05:35 +0800

    自动生成pojo和品牌信息查询测试代码提交

---
+++ pinyougou-parent/pinyougou-sellergoods-interface/src/main/java/com/pinyougou/service/BrandService.java
@@ -0,0 +1,9 @@
+package com.pinyougou.service;
+
+import java.util.List;
+
+import com.pinyougou.pojo.TbBrand;
+
+public interface BrandService {
+	public List<TbBrand> findAll();
+}
---
+++ pinyougou-parent/pinyougou-sellergoods-service/src/main/java/com/pinyougou/sellergoods/service/impl/BrandServiceImpl.java
@@ -0,0 +1,23 @@
+package com.pinyougou.sellergoods.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.pinyougou.mapper.TbBrandMapper;
+import com.pinyougou.pojo.TbBrand;
+import com.pinyougou.service.BrandService;
+
+@Service
+public class BrandServiceImpl implements BrandService {
+	
+	@Autowired
+	private TbBrandMapper brandMapper;
+
+	@Override
+	public List<TbBrand> findAll() {
+		return brandMapper.selectByExample(null);
+	}
+
+}
